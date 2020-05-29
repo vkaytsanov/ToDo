321042bab29c3efa49a4ad38d44ad8c2bf4eed8e
viktor kaytsanov <vkaytsanov@yahoo.com>
2020-05-29 19:23:08 +0300

    fixed settings.py

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
