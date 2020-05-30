f74bda0d77b2ec2e75cc73e15c72ba1be9e459d7
Buldeev <stefan.buldeev@abv.bg>
2020-05-30 16:24:04 +0300

    Added password field to user

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
 
 
 class Achievement(models.Model):
