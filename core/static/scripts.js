741973b51d526ddd73173581d20a9a2d26077674
viktor kaytsanov <vkaytsanov@yahoo.com>
2020-05-30 15:56:58 +0300

    little code cleanup

--- core/static/scripts.js
+++ core/static/scripts.js
@@ -1,7 +1,6 @@
 let opened = false;
 
 $('document').ready(function(){
-
     $('.burger-menu-mobile').on('click',(function(){
         $('.login-section-text a').css({'color' : '#5068a9'});
         $('.login-section').show();
@@ -9,7 +8,6 @@ $('document').ready(function(){
         $('.login-section').show("slide");
         $('.burger-menu-mobile').hide();
         $('.burger-menu-mobile-quit').show();
-        $('li .fas').replaceWith('<a href="" class="notification-replacement">Ð˜Ð·Ð²ÐµÑÑ‚Ð¸Ñ</a>');
         opened = true;
     }));
     $('.burger-menu-mobile-quit').on('click',(function(){
@@ -22,27 +20,4 @@ $('document').ready(function(){
         opened = false;
 
     }));
-
-        $('main').on('click', (function () {
-            if(opened === true) {
-                $('.login-section').hide("slide");
-                $('.burger-menu-mobile-quit').hide();
-                $('.nav-menu').css("display", "none");
-                // $('.burger-menu-mobile i').css("color", "#2c2c2c");
-                $('.login-section-text a').css({'color': '#2c2c2c'});
-                $('.burger-menu-mobile').show();
-                opened = false;
-
-            }
-            if(notif_opened){
-                notif_opened = false;
-                $('#notifPanel').hide();
-            }
-            if(profile_opened){
-                profile_opened = false;
-                $('.login-menu').hide();
-            }
-        }));
-
-
 });
